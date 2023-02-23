bf08884a7c8bfea0eb5390f76632f9f64ceb77f3
Waqas Ahmed <waqas.ghaloo@gmail.com>
2023-02-23 13:14:05 +0000

    Updated Gini Index and wrote function for Info Gain

--- algorithms/classfier.decisiontree.py
+++ algorithms/classfier.decisiontree.py
@@ -4,7 +4,8 @@ import numpy as np
 from sklearn import datasets
 
 def load_dataset():
-    return datasets.load_iris()
+    # returns 2d array with 2 features and 1 target column
+    return np.c_[ datasets.load_iris().data[:,:2], datasets.load_iris().target ]  
 
     # return datasets.load_iris().data[:,:2], datasets.load_iris.target
 
@@ -16,64 +17,97 @@ def load_dataset():
 
 
 def find_feature_cols(data):
-    return data.feature_names[:2]
+    return data.feature_names
 
 def find_unique_values(data, index=0):
     # This function will return array of unique values of a column in 1d or Nd array
     if data.ndim >1:
-        return np.unique(data[:][index]) 
+        print('Nd')
+        return np.unique(data[:,index]) 
     
     return np.unique(data[:])
 
  
-def question(arr,j):
-    condition = arr <= j
+def question(arr,i,j):
+    condition = arr[:,i] <= j
 
     left_subtree = arr[condition]
     right_subtree = arr[~condition]
 
     return left_subtree, right_subtree
 
-def gini(X, y):
-    impurity = 1
-    # class_tup_counts = np.unique(datasets.load_iris().target, return_counts=True)
-    class_counts = {key : value for key, value in zip(list(np.unique(datasets.load_iris().target, return_counts=True)[0]),list(np.unique(datasets.load_iris().target, return_counts=True)[1]))}
+def gini(data):
+
+
 
     impurity = 1
+    # class_tup_counts = np.unique(datasets.load_iris().target, return_counts=True)
+    class_counts = {key : value for key, value in zip(list(np.unique(data[:,-1], return_counts=True)[0]),list(np.unique(data[:,-1], return_counts=True)[1]))}
+
+    # impurity = 1
 
     
     for key, value in class_counts.items():
-        impurity -= (value/len(y))**2
+        impurity -= (value/len(data[:,-1]))**2
 
 
     return impurity
 
 
-def build_tree():
+def info_gain(left_subtree, right_subtree, impurity):
+    # Information Gain uses classes weightage and Entropy/Gini to calculate impurity at node
+    # Information Gain tells how much impurity is being reduced 
+    
+    size = len(left_subtree) + len(right_subtree)
+    ig = impurity - (len(left_subtree)/size) *gini(left_subtree) - (len(right_subtree)/size) *gini(right_subtree)
+
+    return ig
+
+def best_split(data):
     # Extract data in Features and Target label
-    data = load_dataset()
-    X = data.data[:,:2]
-    y = data.target
+    
+    best_gain = 0
+    best_question = None
 
-    impurity = gini(X,y)
+    split_value = 0
+    # X = data.data
+    # y = data.target
 
-
-    for i in range (len(find_feature_cols(data))):
-        for j in find_unique_values(X, i):
-
-            left_subtree, right_subtree = question(X[:][i])
+    impurity = gini(data)
 
 
+    for i in range (data[:,:-1].ndim):
+        for j in find_unique_values(data[:,:-1], i):
+
+            # impurity = gini(X,y)
+
+            left_subtree, right_subtree = question(data,i,j)
+
+            if len(left_subtree) == 0 or len(right_subtree) == 0:
+                continue
+
+            
+            gain = info_gain(left_subtree, right_subtree, impurity)
+
+            if gain>=best_gain:
+                print(gain)
+                best_gain = gain
+                best_question = i
+                split_value = j
+                
 
 
-
-
+    return best_gain, best_question
 
 
     print('Checkpoint')
     # Find question to ask
 
 
-build_tree()
+data = load_dataset()
+# X = data.data[:,:2]
+# y = data.target
+
+best_split(data)
 
 # def question()
